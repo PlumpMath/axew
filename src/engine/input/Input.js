413ac9ceea293eeebeabb8ff5dc0936b36bb91ae
Vinay Shenoy <vinaysshenoy@gmail.com>
2017-07-02 13:00:26 +0530

    Added event for key up in Input

--- src/engine/input/Input.js
+++ src/engine/input/Input.js
@@ -33,7 +33,9 @@ const Input = {
     r_meta: false
   },
   //Maintains a map of keys to action states
-  keys: {}
+  keys: {},
+  //Used for event based input
+  listeners: {}
 };
 
 function findModifierForEvent(event) {
@@ -70,36 +72,13 @@ function findModifierForEvent(event) {
   }
 }
 
-function setup() {
-  document.addEventListener('keydown', event => {
-    //Update modifier state
-    const modifier = findModifierForEvent(event);
-    if (modifier !== null) {
-      Input.modifiers[modifier] = true;
-    }
-
-    const key = event.key;
-    if(!hasProperty(Input.keys, key)) {
-      Input.keys[key] = ActionStates.DOWN;
-    }
-  });
-  document.addEventListener('keyup', event => {
-    //Update modifier state
-    const modifier = findModifierForEvent(event);
-    if (modifier !== null) {
-      Input.modifiers[modifier] = false;
-    }
-    const key = event.key;
-    if(hasProperty(Input.keys, key)) {
-      const actionState = Input.keys[key];
-      if(actionState === ActionStates.DOWN || actionState === ActionStates.PRESSED) {
-        Input.keys[key] = ActionStates.UP;
-      }
-    }
-  });
+function dispatchEvent(type, payload) {
+  if (hasProperty(Input.listeners, type)) {
+    Input.listeners[type].forEach(listener => {
+      listener.listener.call(listener.scope, payload);
+    });
+  }
 }
-
-setup();
 
 const api = Object.freeze({
   modifier: function (code) {
@@ -110,41 +89,121 @@ const api = Object.freeze({
     return false;
   },
   keyDown: function (key) {
-    if(hasProperty(Input.keys, key)) {
+    if (hasProperty(Input.keys, key)) {
       return Input.keys[key] === ActionStates.DOWN;
     }
 
     return false;
   },
   keyUp: function (key) {
-    if(hasProperty(Input.keys, key)) {
+    if (hasProperty(Input.keys, key)) {
       return Input.keys[key] === ActionStates.UP;
     }
 
     return false;
   },
   key: function (key) {
-    if(hasProperty(Input.keys, key)) {
+    if (hasProperty(Input.keys, key)) {
       return Input.keys[key] === ActionStates.DOWN || Input.keys[key] === ActionStates.PRESSED;
     }
 
     return false;
   },
+  registerEventListener: function (type, listener, scope) {
+    if (!hasProperty(Input.listeners, type)) {
+      Input.listeners[type] = [];
+    }
+
+    Input.listeners[type].push({
+      listener: listener,
+      scope: scope
+    });
+  },
+  unregisterEventListener: function (type, listener) {
+    if (hasProperty(Input.listeners, type)) {
+      const stack = Input.listeners[type];
+      for (let stackIndex = 0; stackIndex < stack.length; stackIndex++) {
+        if (listener === stack[stackIndex].listener) {
+          stack.splice(stackIndex, 1);
+          break;
+        }
+      }
+    }
+  },
   //For internal use only
-  onFrameEnd: function() {
+  onFrameEnd: function () {
     let actionState;
-    for(let key in Input.keys) {
-      if(hasProperty(Input.keys, key)) {
+    for (let key in Input.keys) {
+      if (hasProperty(Input.keys, key)) {
         actionState = Input.keys[key];
-        if(ActionStates.UP === actionState) {
+        if (ActionStates.UP === actionState) {
           delete Input.keys[key];
-        } else if(ActionStates.DOWN === actionState) {
+        } else if (ActionStates.DOWN === actionState) {
           Input.keys[key] = ActionStates.PRESSED;
         }
       }
     }
   }
 });
+
+function createKeyEvent(key) {
+  return {
+    key: key,
+    lShift: api.modifier(Modifiers.LSHIFT),
+    rShift: api.modifier(Modifiers.RSHIFT),
+    lControl: api.modifier(Modifiers.LCONTROL),
+    rControl: api.modifier(Modifiers.RCONTROL),
+    lAlt: api.modifier(Modifiers.LALT),
+    rAlt: api.modifier(Modifiers.RALT),
+    lMeta: api.modifier(Modifiers.LMETA),
+    rMeta: api.modifier(Modifiers.RMETA),
+    get shift() {
+      return this.lShift || this.rShift;
+    },
+    get control() {
+      return this.lControl || this.rControl;
+    },
+    get alt() {
+      return this.lAlt || this.rAlt;
+    },
+    get meta() {
+      return this.lMeta || this.rMeta;
+    }
+  }
+}
+
+function setup() {
+  document.addEventListener('keydown', event => {
+    //Update modifier state
+    const modifier = findModifierForEvent(event);
+    if (modifier !== null) {
+      Input.modifiers[modifier] = true;
+    }
+
+    const key = event.key;
+    if (!hasProperty(Input.keys, key)) {
+      Input.keys[key] = ActionStates.DOWN;
+    }
+  });
+  document.addEventListener('keyup', event => {
+    //Update modifier state
+    const modifier = findModifierForEvent(event);
+    if (modifier !== null) {
+      Input.modifiers[modifier] = false;
+    }
+    const key = event.key;
+    if (hasProperty(Input.keys, key)) {
+      const actionState = Input.keys[key];
+      if (actionState === ActionStates.DOWN || actionState === ActionStates.PRESSED) {
+        Input.keys[key] = ActionStates.UP;
+      }
+    }
+
+    dispatchEvent('key', createKeyEvent(key));
+  });
+}
+
+setup();
 
 export {
   ActionStates,
--- src/game/Controller.js
+++ src/game/Controller.js
@@ -1,34 +1,19 @@
 "use strict";
 import { Modifiers, Input } from '../engine/input/Input';
 import Entity from '../engine/core/Entity';
-import KeyboardInputTracker from '../engine/input/KeyboardInputTracker';
 
 export default class Controller extends Entity {
   constructor() {
     super('_controller');
-    this.keyboardInputTracker = new KeyboardInputTracker();
+    Input.registerEventListener('key', (event) => {
+      console.log(event);
+      console.log(this);
+    }, this);
   }
 
   bindToGame(game) {
-    // this.keyboardInputTracker.listenForEvents(game);
   }
 
   onUpdate() {
-
-    if(Input.key('ArrowRight')) {
-      console.log('Arrow Right');
-    }
-
-    if(Input.key(' ')) {
-      console.log('Space');
-    }
-
-    if(Input.key('Enter')) {
-      console.log('Enter');
-    }
-
-    if(Input.key('Meta')) {
-      console.log('Meta');
-    }
   }
 }
